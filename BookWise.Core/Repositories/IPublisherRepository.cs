612440f70411cb3d1035ca40a3894bb70976158d
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-10-31 18:00:17 -0300

    Criando interface publisherRepository

---
+++ BookWise.Core/Repositories/IPublisherRepository.cs
@@ -0,0 +1,13 @@
+using BookWise.Core.Entities;
+
+namespace BookWise.Core.Repositories;
+
+public interface IPublisherRepository
+{
+    Task AddAsync(Publisher publisher);
+    Task<Publisher?> GetByIdAsync(int id);
+    Task<bool> ExistsByIdAsync(int id);
+    Task<IQueryable<Publisher>> GetPaginatedAsync(string search, int page, int size);
+    Task DeleteAsync(int id);
+    void Update(Publisher publisher);
+}
