d14a634c2dbd064a5af746cd2787c4835aa9444e
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-10-31 22:21:01 -0300

    Refatorando nome das propriedades

--- BookWise.Core/Entities/Loan.cs
+++ BookWise.Core/Entities/Loan.cs
@@ -4,7 +4,7 @@ namespace BookWise.Core.Entities;
 
 public class Loan : BaseEntity
 {
-    public Loan(DateTime loanDate, decimal value, DateTime returnDate)
+    public Loan(DateTime loanDate, decimal value, DateTime returnDate, int userId, int bookId)
     {
         if (returnDate < loanDate)
         {
@@ -13,14 +13,16 @@ public class Loan : BaseEntity
         LoanDate = loanDate;
         Value = value;
         ReturnDate = returnDate;
+        UserId = userId;
+        BookId = bookId;
     }
 
     public DateTime LoanDate { get; private set; }
-    public DateTime ReturnDate { get; set; }
+    public DateTime ReturnDate { get; private set; }
     public decimal Value { get; private set; }
-    public int IdUser { get; private set; }
+    public int UserId { get; private set; }
     public User User { get; private set; }
-    public int IdBook { get; private set; }
+    public int BookId { get; private set; }
     public Book Book { get; private set; }
-    public EnumLoanStatus Status { get; set; }
+    public EnumLoanStatus Status { get; private set; }
 }
