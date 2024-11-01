ceaa71ea8c2d322da84ad3d8ce380ffea90ead53
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-11-01 20:45:57 -0300

    Implementando RatingsRepository

--- BookWise.Core/Repositories/IRatingRepository.cs
+++ BookWise.Core/Repositories/IRatingRepository.cs
@@ -5,6 +5,6 @@ namespace BookWise.Core.Repositories;
 public interface IRatingRepository
 {
     Task AddAsync(Rating rating);
-    Task<IQueryable<Rating>> GetRatingsByBookIdAsync(int bookId, int page, int size);
+    Task<IEnumerable<Rating>> GetRatingsByBookIdAsync(int bookId, int page, int size);
     Task<bool> ExistsByIdAsync(int id);
 }
