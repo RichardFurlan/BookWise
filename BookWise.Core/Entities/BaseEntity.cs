bac926b459e25229c008463c03c68a4a025068a6
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-10-31 22:21:50 -0300

    Criando metodo para definir metodo update

--- BookWise.Core/Entities/BaseEntity.cs
+++ BookWise.Core/Entities/BaseEntity.cs
@@ -5,15 +5,26 @@ public abstract class BaseEntity
     public BaseEntity()
     {
         CreatedAt = DateTime.Now.ToUniversalTime();
+        UpdateAt = DateTime.Now.ToUniversalTime();
         IsDeleted = false;
+        IsUpdated = false;
     }
 
     public int Id { get; private set; }
     public DateTime CreatedAt { get; private set; }
+    public DateTime UpdateAt { get; private set; }
+    public bool IsUpdated { get; private set; }
     public bool IsDeleted { get; private set; }
 
     public void SetAsDeleted()
     {
         IsDeleted = true;
+        MarkAsUpdated();
+    }
+
+    public void MarkAsUpdated()
+    {
+        IsUpdated = true;
+        UpdateAt = DateTime.UtcNow;
     }
 }
