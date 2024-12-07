0eff221e7a1885fb9b42e13f1fe911422abf33c6
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-12-07 20:55:38 -0300

    Adicionando Update ao BookDomainService

--- BookWise.Core/Services/BookDomainService.cs
+++ BookWise.Core/Services/BookDomainService.cs
@@ -1,4 +1,6 @@
 using BookWise.Core.Entities;
+using BookWise.Core.Enum;
+using BookWise.Core.Exceptions;
 using BookWise.Core.Repositories;
 
 namespace BookWise.Core.Services;
@@ -18,12 +20,33 @@ public class BookDomainService
         if (authors.Count != authorIds.Count)
         {
             var missingIds = authorIds.Except(authors.Select(a => a.Id)).ToList();
-            throw new KeyNotFoundException($"Os seguintes autores não foram encontrados: {string.Join(", ", missingIds)}"); 
+            throw new DomainException($"Os seguintes autores não foram encontrados: {string.Join(", ", missingIds)}"); 
         }
         
         foreach (var author in authors)
         {
             book.AddAuthor(author);
         }
+    }
+
+    public void UpdateBook(
+        Book book,
+        string title,
+        string isbn,
+        DateTime publicationDate,
+        int edition,
+        EnumLenguage lenguage,
+        int numberOfPage)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new DomainException("O título não pode ser vazio.");
+        
+        if (edition <= 0)
+            throw new DomainException("A edição deve ser maior que zero.");
+        
+        if (numberOfPage <= 0)
+            throw new DomainException("O número de páginas deve ser maior que zero.");
+
+        book.UpdateDetails(title, isbn, edition, publicationDate ,lenguage, numberOfPage);
     }
 }
